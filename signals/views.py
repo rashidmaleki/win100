f074d880896343d9d69d117cf0c9813f01dfbee6
rashidmaleki <60543596+rashidmaleki@users.noreply.github.com>
2022-12-26 17:42:05 +0330

    Update views.py

--- signals/views.py
+++ signals/views.py
@@ -1,13 +1,10 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
 from signals.serializers import SignalSerializer
 from signals.models import Signal
-from rest_framework import mixins
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets
 
 User = get_user_model()
 
-class SignalViewSet(generics.ListCreateAPIView):
+class SignalViewSet(viewsets.ModelViewSet):
     queryset = Signal.objects.all()
     serializer_class = SignalSerializer
