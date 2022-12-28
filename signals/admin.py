f6a1614c68d71333cd64d54ffc1b349fdb3db289
rashidmaleki <60543596+rashidmaleki@users.noreply.github.com>
2022-12-28 18:10:12 +0330

    pep8

--- signals/admin.py
+++ signals/admin.py
@@ -5,7 +5,8 @@ from signals.models import Signal, Currency
 # Register your models here.
 @admin.register(Signal)
 class SignalAdmin(admin.ModelAdmin):
-    list_display = ('currency', 'price', 'get_plans', 'presentation_time', 'entry_point', 'loss_limit')
+    list_display = ('currency', 'price', 'get_plans',
+                    'presentation_time', 'entry_point', 'loss_limit', 'status')
     list_filter = (
         ('created'),
         ('currency'),
@@ -13,12 +14,10 @@ class SignalAdmin(admin.ModelAdmin):
         ('plan'),
         ('entry_point'),
         ('loss_limit'),
+        ('status'),
     )
-
 
 
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
     pass
-
-
--- signals/models.py
+++ signals/models.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from accounts.models import Plan
 
 # Create your models here.
+
+
 class Currency(models.Model):
     name = models.CharField(verbose_name='نام ارز', max_length=50)
     code = models.CharField(verbose_name='اختصار', max_length=50)
@@ -17,19 +19,29 @@ class Currency(models.Model):
 
 
 class Signal(models.Model):
-    currency = models.ForeignKey(Currency, verbose_name=("نام ارز"), on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, verbose_name=(
+        "نام ارز"), on_delete=models.CASCADE)
     price = models.IntegerField(verbose_name=("قیمت فعلی"))
-    presentation_time = models.DateTimeField(verbose_name=("تاریخ و ساعت ارائه"))
+    presentation_time = models.DateTimeField(
+        verbose_name=("تاریخ و ساعت ارائه"))
     entry_point = models.FloatField(verbose_name=("نقطه ورود"))
-    loss_limit = models.FloatField(blank=True, null=True, verbose_name=("حد ضرر"))
-    target1 = models.FloatField(blank=True, null=True, verbose_name=("تارگت اول"))
-    target2 = models.FloatField(blank=True, null=True, verbose_name=("تارگت دوم"))
-    target3 = models.FloatField(blank=True, null=True, verbose_name=("تارگت سوم"))
-    target4 = models.FloatField(blank=True, null=True, verbose_name=("تارگت چهارم"))
-    target5 = models.FloatField(blank=True, null=True, verbose_name=("تارگت پنجم"))
+    loss_limit = models.FloatField(
+        blank=True, null=True, verbose_name=("حد ضرر"))
+    target1 = models.FloatField(
+        blank=True, null=True, verbose_name=("تارگت اول"))
+    target2 = models.FloatField(
+        blank=True, null=True, verbose_name=("تارگت دوم"))
+    target3 = models.FloatField(
+        blank=True, null=True, verbose_name=("تارگت سوم"))
+    target4 = models.FloatField(
+        blank=True, null=True, verbose_name=("تارگت چهارم"))
+    target5 = models.FloatField(
+        blank=True, null=True, verbose_name=("تارگت پنجم"))
     lever = models.FloatField(blank=True, null=True, verbose_name=("اهرم"))
     plan = models.ManyToManyField(Plan, verbose_name=("پکیج"))
-    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ثبت سیگنال')
+    status = models.BooleanField(default=True, verbose_name='فعال')
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name='تاریخ ثبت سیگنال')
     edited = models.DateTimeField(auto_now=True)
 
     @admin.display(description="پکیج")
@@ -39,7 +51,6 @@ class Signal(models.Model):
     def __str__(self) -> str:
         return f'سیگنال شماره {self.id}'
 
-    
     class Meta:
         verbose_name_plural = "سیگنال ها"
         verbose_name = "سیگنال"
